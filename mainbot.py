b45ad681fa48256bcf16f18f33640d6f917e3183
NOLAWorks Community <mmennelle@nolaworks.org>
2025-04-05 16:06:45 -0500

    made the signout message more accepting of people who cant read 24hr time.

--- mainbot.py
+++ mainbot.py
@@ -160,7 +160,7 @@ async def signout(interaction: discord.Interaction, time: str):
 
     data["tools"][tool]["reservations"].append({"user": interaction.user.name, "time": formatted_time})
     save_tools(data)
-    await interaction.followup.send(f"{tool} signed out for {formatted_time} by {interaction.user.name}!")
+    await interaction.followup.send(f"Signed out -- {time} -- by {interaction.user.name}! -- {formatted_time}")
 
 @bot.tree.command(name="returntool", description="Return a tool")
 async def tool_return(interaction: discord.Interaction):
